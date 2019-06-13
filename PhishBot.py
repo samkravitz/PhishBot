56f9d7bf2dec810929c513e60a254aaaab68e9c6
Sam Kravitz <sam.kravitz@coxautoinc.com>
2019-06-13 08:31:06 -0400

    Add musixmatch API functionality

--- PhishBot.py
+++ PhishBot.py
@@ -6,46 +6,102 @@
 import tweepy
 import time
 import random
+from musixmatch import Musixmatch
+
+musixmatch = Musixmatch('<key>')
 
 #values changed for security purposes.
 #access apps.twitter.com to find the values for your specific project
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler('<consumer_key>', '<consumer_secret>')
+auth.set_access_token('<access_token>', '<access_token_secret>')
 
 #authorizes the bot and loads in its username
 api = tweepy.API(auth)
 user = api.get_user('Phish_Bot')
-
-f= open('lyrics.txt', 'r')
-
-#array that contains every line from lyrics.txt as a sepparate element
-myList = []
-for line in f:
-    myList.append(line)
-
-f.close()
 
 #at the moment the bot also follows follower of my main account, @krav1tz
 #myFollowers is an array containing each user who follows @krav1tz
 myFollowers = api.followers_ids('krav1tz')
 myFollowersLength = len(myFollowers)
 
-counter = myFollowersLength - 105
+#artist
+phish = musixmatch.artist_search('phish', 1, 1, 0, 0)
+phish_id = phish['message']['body']['artist_list'][0]['artist']['artist_id']
 
-#followUser: follows the user who is located at a given index in my list of followers
-#@param: integer value of index of user
-#@return: void
-def followUser(userIndex):
-    api.create_friendship(myFollowers[userIndex])
+#albums
+albums = musixmatch.artist_albums_get(phish_id,0,1,100,0)
+album_list = albums['message']['body']['album_list']
+
+new_album_list = []
+new_album_dic = {}
+
+# filter live albums
+for album in album_list:
+    name = album['album']['album_name']
+    if name[:1].isdigit():
+        pass
+    elif 'Phish' in name:
+        pass
+    elif 'Live' in name:
+        pass
+    elif 'At' in name:
+        pass
+    elif "'" in name:
+        pass
+    elif 'Want to Do' in name:
+        pass
+    elif name not in new_album_dic:
+        new_album_dic[name] = album
+        new_album_list.append(album)
+
+def getTweet():
+    num_albums = len(new_album_list)
+    albums = new_album_list
+    random_album = albums[random.randint(0, num_albums - 1)]
+
+    # tracks
+    tracks = musixmatch.album_tracks_get(random_album['album']['album_id'], 1, 100, 0)
+    tracks = tracks['message']['body']['track_list']
+    num_tracks = len(tracks)
+    rand = int(random.randint(0, num_tracks - 1))
+    random_track = tracks[rand]
+
+    # lyrics
+    lyrics = musixmatch.track_lyrics_get(random_track['track']['track_id'])
+    lyrics = lyrics['message']['body']['lyrics']['lyrics_body']
+
+    lyricsf = ''
+    for line in lyrics.split('\n'):
+        if '...' in line:
+            pass
+        elif 'Lyrics' in line:
+            pass
+        elif '(' in line:
+            pass
+        elif not line.strip() == '':
+            lyricsf += line + '\n'
+
+    lyricsf = lyricsf.rstrip()
+
+    lines = lyricsf.splitlines()
+    nlines = len(lines)
+    rline = random.randint(0, nlines - 1)
+
+    tweet = ''
+
+    while rline < nlines and len(tweet) < 150:
+        tweet += lines[rline]
+        tweet += '\n'
+        rline += 1
+    return tweet.rstrip()    
+
 
 #this loop goes until all of my followers have been followed, will be changed later to run indefinitely
-while (counter > 0):
-    toTweet = random.choice(myList)
+while (True):
+    tweet = getTweet()
     try:
-        api.update_status(toTweet)
+        api.update_status(tweet)
     except tweepy.error.TweepError:
-        api.update_status(random.choice(myList))
-    followUser(counter)
-    print(api.get_user(myFollowers[counter]).screen_name)
-    counter = counter - 1
+        api.update_status(getTweet)
+    
     time.sleep(1800)
